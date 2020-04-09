bb5339873ba3ebc0474c7eb282e4a3fc2e7b61cb
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-04-09 18:55:50 -0400

    initialized server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
+const Koa = require('koa');
 
+// Create app
+
+const app = new Koa();
+const PORT = 3000;
+
+// Start app...
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
